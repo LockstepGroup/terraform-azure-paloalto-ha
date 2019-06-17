d5b21b909e26e69153e3125c1faff14008874e6e
Brian Addicks <brian@addicks.us>
2019-06-17 13:22:36 -0400

    added output for availability set

--- outputs.tf
+++ outputs.tf
@@ -10,3 +10,6 @@ output "primary_pa_outside_ip_address" {
 output "primary_pa_outside_public_ip_address" {
   value = "${module.primary_pa.outside_pip}"
 }
+output "availability_set_id" {
+  value = "${module.shared_resources.availability_set_id}"
+}
